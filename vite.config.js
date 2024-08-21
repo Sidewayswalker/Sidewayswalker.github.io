1658ddfe39e8a0801f289900d1df41d24f0ff15c
Steven Karl <Karl.Steven09@gmail.com>
2024-08-21 12:56:34 -0500

    working on code to fix for github

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
     return {
-        base: './',  // Add this line
         build: {
             outDir: 'build',
         },
         server: {
             proxy: {
-                "/api": 'http://localhost:5001',
+                "/api":'http://localhost:5001',
             }
         },
         plugins: [react()],
+        base: "/Sidewayswalker.github.io/"
     };
 });
