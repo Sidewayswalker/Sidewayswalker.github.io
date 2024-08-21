44590f453bd2221cfc0598b72fb633b5e64e058f
Steven Karl <Karl.Steven09@gmail.com>
2024-08-21 09:58:57 -0500

    changed vite.config.js

--- src/components/TechnicalSkills/TechnicalSkills.jsx
+++ src/components/TechnicalSkills/TechnicalSkills.jsx
@@ -5,15 +5,13 @@ function TechnicalSkills() {
   return (
     <div className="container">
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur
-            id felis metus. Vestibulum et pulvinar tortor. Morbi pharetra lacus
-            ut ex molestie blandit. Etiam et turpis sit amet risus mollis
-            interdum. Suspendisse et justo vitae metus bibendum fringilla sed
-            sed justo. Aliquam sollicitudin dapibus lectus, vitae consequat odio
-            elementum eget. Praesent efficitur eros vitae nunc interdum, eu
-            interdum justo facilisis. Sed pulvinar nulla ac dignissim efficitur.
-            Quisque eget eros metus. Vestibulum bibendum fringilla nibh a
-            luctus. Duis a sapien metus.
+            Javascript, 
+              <ul>React</ul>
+              <ul>Redux</ul>
+              <ul>Node.js</ul>
+              <ul></ul>
+              <ul></ul>
+            Express.js, SQL, HTML, CSS, Git/version control, C#, .NET, RESTful API
           </p>
     </div>
   );
--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
     return {
+        base: './',  // Add this line
         build: {
             outDir: 'build',
         },
         server: {
             proxy: {
-                "/api":'http://localhost:5001',
+                "/api": 'http://localhost:5001',
             }
         },
         plugins: [react()],
