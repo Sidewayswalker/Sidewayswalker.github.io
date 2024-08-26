95cc59c9143441293adf4f112062dc6c4a14fd29
Steven Karl <Karl.Steven09@gmail.com>
2024-08-26 18:17:54 -0500

    forgot a word

--- src/components/TechnicalSkills/TechnicalSkills.jsx
+++ src/components/TechnicalSkills/TechnicalSkills.jsx
@@ -8,30 +8,31 @@ function TechnicalSkills() {
       <p className='technical-skills-description'>Below is a list of my skills and my comfortability with them.</p>
       <div className='tech-skills'>
         <h3>Languages</h3>
-        <p>Javascript</p>
+          <p>Javascript</p>
 
         <div className='front-back-container'>
           <div className='front-back-column'>
             <h3>Front End</h3>
-            <p>HTML</p>
-            <p>CSS</p>
-            <p>React</p>
-            <p>Redux</p>
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>React</p>
+              <p>Redux</p>
           </div>
           <div className='front-back-column'>
             <h3>Back End</h3>
-            <p>Node.js</p>
-            <p>Express.js</p>
-            <p>C#</p>
-            <p>.NET</p>
+              <p>Node.js</p>
+              <p>Express.js</p>
+              <p>C#</p>
+              <p>.NET</p>
           </div>
         </div>
 
         <h3>Database Management</h3>
-        <p>SQL</p>
+          <p>SQL</p>
         <h3>Version Control</h3>
-        <p>Git</p>
-        <h3>RESTful API</h3>
+          <p>Git</p>
+        <h3>API Development</h3>
+          <p>RESTful API</p>
       </div>
       <div className='more-coming-soon'>
         <p>MORE COMING SOON!</p>
