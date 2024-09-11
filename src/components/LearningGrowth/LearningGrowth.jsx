e8e572935d1f27ed2f071360c1ed8ccaf83a869d
Steven Karl <Karl.Steven09@gmail.com>
2024-09-11 14:40:52 -0500

    updated Learning & Growth

--- src/components/LearningGrowth/LearningGrowth.jsx
+++ src/components/LearningGrowth/LearningGrowth.jsx
@@ -7,17 +7,19 @@ function LearningGrowth() {
       <h2 className='project-board-title'>Learning & Growth</h2>
         <p>This is where I will be displaying all of the activities & topics for the corresponding months that i will be investigating or learning more about. There will also be expansive links to Youtube Videos, Online Courses & other helpful links.</p>
     <h3>July 2024</h3>
-    <li><b>Graduated from Prime Digital Academy</b></li>
-    <li>Touch Up & Refactor Old Projects</li>
-    <li>Mentor Meetings</li>
-    <li>Interview Practice</li>
-    <li>Tech Terminology</li>
-    <li>Apply for Jobs</li>
+      <li><b>Graduated from Prime Digital Academy</b></li>
+      <li>Touch Up & Refactor Old Projects</li>
+      <li>Mentor Meetings</li>
+      <li>Interview Practice</li>
+      <li>Tech Terminology</li>
+      <li>Apply for Jobs</li>
     <h3>August 2024</h3>
-    <li>Upgrade Personal Website</li>
-    <li>Networking Events</li>
+      <li>Upgrade Personal Website</li>
+      <li>Networking Events</li>
     <h3>September 2024</h3>
-    <li>TBD</li>
+      <li>Applying to Jobs</li>
+      <li>Networking & Meetups</li>
+    <li></li>
     <h3>October 2024</h3>
     <li>TBD</li>
     <h3>November 2024</h3>
